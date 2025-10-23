d67f1deb30264f5666b0613c87872f5f1cc66d7f
pedro00876 <pcchagas00@gmail.com>
2025-10-23 16:28:03 -0300

    Diretiva v-for

---
+++ vue-project/src/components/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-for="obj in todos" v-bind:key="obj.id">
+    {{ obj.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      todos: [
+        {
+          userId: 1,
+          id: 1,
+          title: 'delectus aut autem',
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 2,
+          title: 'quis ut nam facilis et officia qui',
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 3,
+          title: 'fugiat veniam minus',
+          completed: false,
+        },
+        {
+          userId: 1,
+          id: 4,
+          title: 'et porro tempora',
+          completed: true,
+        },
+        {
+          userId: 1,
+          id: 5,
+          title:
+            'laboriosam mollitia et enim quasi adipisci quia provident illum',
+          completed: false,
+        },
+      ],
+    }
+  },
+}
+</script>
